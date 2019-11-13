4aac1fe740d0fd7ae7c5f58b675cdde64254b8a7
Jiazhi Guo <jerrykuo7727@gmail.com>
2019-11-14 00:30:23 +0800

    Fix bug of shell script recipe.

--- run.sh
+++ run.sh
@@ -3,23 +3,25 @@
 python3_cmd=python3
 
 stage=0
-use_gpu=cuda:2
+use_gpu=cuda:1
 
 model=bert  # (bert|xlnet)
-model_path=/home/M10815022/Models/roberta-wwm-ext
-save_path=./models/roberta
-dataset="DRCD Lee Kaggle ASR"
+model_path=/home/M10815022/Models/bert-wwm-ext
+save_path=./models/bert
+datasets="DRCD Lee Kaggle ASR"
 
 
 if [ $stage -le 0 ]; then
   echo "==================================================="
   echo "     Convert traditional Chinese to simplified     "
   echo "==================================================="
-  for dataset in $dataset FGC; do
+  for dataset in $datasets FGC; do
     for split in training dev test; do
       file=dataset/$dataset/${dataset}_${split}.json
-      echo "Converting '$file'..."
-      opencc -i $file -o $file -c t2s.json
+      if [ -f $file ]; then
+        echo "Converting '$file'..."
+        opencc -i $file -o $file -c t2s.json
+      fi
     done
   done
   echo "Done."
@@ -36,7 +38,7 @@ if [ $stage -le 1 ]; then
       mkdir -p data/$split/$dir
     done
   done
-  $python3_cmd scripts/prepare_${model}_data.py $model_path $dataset FGC || exit 1
+  $python3_cmd scripts/prepare_${model}_data.py $model_path $datasets FGC || exit 1
 fi
 
 
@@ -48,5 +50,5 @@ if [ $stage -le 2 ]; then
     echo "'$save_path' already exists! Please remove it and try again."; exit 1
   fi
   mkdir -p $save_path
-  $python3_cmd scripts/train_${model}.py $use_gpu $model_path $save_path $dataset
+  $python3_cmd scripts/train_${model}.py $use_gpu $model_path $save_path $datasets
 fi
